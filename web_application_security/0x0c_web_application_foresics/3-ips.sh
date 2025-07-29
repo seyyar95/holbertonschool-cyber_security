3531bdc38035a94da9fe08d1412f85770a733c45
seyyar95 <heydrovsyyar@gmail.com>
2025-07-29 17:26:19 +0400

    I changed the command completely

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat auth.log | awk '/Accepted password/ {for (i = 1; i <= NF; i++) if ($i == "from") ip=$(i+1) IPS[ip]++} END {print length(IPS)}'
+cat auth.log | awk '/Accepted password for root/ {for (i = 1; i < NF; i++) if ($i == "from") ip=$(i+1); if (ip != "") IPS[ip]++} END {for (i in IPS) print i}'
