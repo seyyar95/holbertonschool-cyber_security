0ab5f35d628b9494600802c465112e43957125e4
seyyar95 <heydrovsyyar@gmail.com>
2025-07-30 16:55:18 +0400

    Don't know how to do that

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat auth.log | awk '/useradd/ {for (i = 0; i <= NF; i++) if ($i == "user:") split($(i+1),a,"=") u[a[2]]} END {for (i in u) print i }' | sort | awk '{printf $1}' | sed 's/,$/ /'
+cat auth.log | awk '/useradd/' | cut -d " " -f 8 | awk -F'=' '{printf $2}' | sort | sed 's/,$//'
