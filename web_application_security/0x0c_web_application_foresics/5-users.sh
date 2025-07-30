e4b751600595ff1d41d3966d0014ded53995b2d8
seyyar95 <heydrovsyyar@gmail.com>
2025-07-30 17:00:16 +0400

    added new line

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat auth.log | awk '/useradd/' | cut -d " " -f 8 | sort | uniq | awk -F'=' '{printf $2}' | sed 's/,$//'
+cat auth.log | awk '/useradd/' | cut -d " " -f 8 | sort | uniq | awk -F'=' '{printf $2}' | sed 's/,$/\n/'
