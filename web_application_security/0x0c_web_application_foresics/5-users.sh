72d364830de50f6517810ef6005242981049017c
seyyar95 <heydrovsyyar@gmail.com>
2025-07-30 14:43:59 +0400

    I found users

---
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat auth.log | awk '/useradd/ {for (i = 0; i <= NF; i++) if ($i == "user:") split($(i+1),a,"=") u[a[2]]} END {for (i in u) print i }' | sort >  names.txt && cat names.txt | awk '{printf $1}' | sed 's/,$//'
