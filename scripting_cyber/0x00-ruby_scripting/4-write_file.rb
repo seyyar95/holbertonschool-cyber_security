661ef8673955d4788e1417e20e5590d78919fa23
seyyar95 <heydrovsyyar@gmail.com>
2025-08-30 14:34:35 +0400

    desired string is added

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -13,4 +13,5 @@ def merge_json_files(file1_path, file2_path)
   File.open(file2_path, "w") do | file |
     file.write(JSON.pretty_generate(merged_hash))
   end
+  puts "Merged JSON written to file.json"
 end
