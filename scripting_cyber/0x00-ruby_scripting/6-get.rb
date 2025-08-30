e49736f6a66da8faa5b5ee0cf454aa44a2cbab02
seyyar95 <heydrovsyyar@gmail.com>
2025-08-30 15:36:07 +0400

    new line added

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -4,5 +4,5 @@ require 'net/http'
 def get_request(url)
   uri = URI.parse(url)
   res = Net::HTTP.get_response(uri)
-  puts "Response status: #{res.code} #{res.message}\nResponse body: #{res.body}"
+  puts "Response status: #{res.code} #{res.message}\nResponse body:\n#{res.body}"
 end
