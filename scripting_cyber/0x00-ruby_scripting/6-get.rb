2b6ca348fe3a4d89ebb7c2c9d44585d961effd5f
seyyar95 <heydrovsyyar@gmail.com>
2025-08-30 15:39:14 +0400

    parsed JSON in a pretty way

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'json'
 
 def get_request(url)
   uri = URI.parse(url)
   res = Net::HTTP.get_response(uri)
-  puts "Response status: #{res.code} #{res.message}\nResponse body:\n#{res.body}"
+  body = JSON.pretty_generate(res.body)
+  puts "Response status: #{res.code} #{res.message}\nResponse body:\n#{body}"
 end
