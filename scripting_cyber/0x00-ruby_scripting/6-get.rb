3343893a1f7c68ed340572443de07bd66efad140
seyyar95 <heydrovsyyar@gmail.com>
2025-08-30 15:34:57 +0400

    changed some funcs

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -3,7 +3,6 @@ require 'net/http'
 
 def get_request(url)
   uri = URI.parse(url)
-  req = Net::HTTP::Get.new(uri.to_s)
-  res = Net::HTTP.start(uri.host, uri.port) { | http | http.request(req)}
-  puts "Response status: #{res.code} OK\nResponse body: #{res.body}"
+  res = Net::HTTP.get_response(uri)
+  puts "Response status: #{res.code} #{res.message}\nResponse body: #{res.body}"
 end
